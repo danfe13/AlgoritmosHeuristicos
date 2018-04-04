591c39b49db460b63fd2c6b3dca9c22a8841126a
Rodrigo Fontes <rodrifontes@gmail.com>
2018-04-04 15:15:58 -0300

    Benchmark Function

---
+++ AlgoritmosHeuristicos/src/br/ufs/benchmarkFunctions/SphereFunctionHillClimbing.java
@@ -0,0 +1,22 @@
+package br.ufs.benchmarkFunctions;
+
+import br.ufs.algorithm.HillClimbing;
+
+public class SphereFunctionHillClimbing extends HillClimbing{
+
+	public double[] initSolution(int length) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double[] tweak(double[] s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double quality(double[] s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
